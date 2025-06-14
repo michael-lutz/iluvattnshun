chore: fixed issue with data leakage, trains now

examples/var_rename.py
@@ -18,6 +18,10 @@ from iluvattnshun.types import TensorTree
 class VariableRenamingConfig(PromptConfig, TrainerConfig):
     """Configuration for variable renaming prompts."""
 
+    train_size: int
+    """Number of training examples."""
+    val_size: int
+    """Number of validation examples."""
     num_chains: int
     """Number of independent renaming chains."""
     chain_length: int
@@ -152,7 +156,8 @@ class MultilayerTransformer(nn.Module):
 class VariableRenamingTrainer(Trainer[VariableRenamingConfig]):
     def get_model(self) -> nn.Module:
         """Get the model."""
-        return MultilayerTransformer(n_layers=3)
+        max_seq_len = self.config.chain_length * self.config.num_chains * 4 + 2
+        return MultilayerTransformer(n_layers=9, max_seq_len=max_seq_len)
 
     def get_optimizer(self, model: nn.Module) -> optim.Optimizer:
         """Returns a basic Adam optimizer."""
@@ -184,7 +189,7 @@ class VariableRenamingTrainer(Trainer[VariableRenamingConfig]):
         train_path = Path(self.config.dataset_path) / "train"
         if not os.path.exists(train_path):
             prompter = VariableRenamingPrompter(self.config)
-            prompter.make_dataset(train_path.as_posix())
+            prompter.make_dataset(train_path.as_posix(), size=self.config.train_size, seed=24)
 
         dataset = Dataset.load_from_disk(train_path.as_posix())
         dataset.set_format(type="torch", columns=["prompt_tokens", "answer_tokens", "prompt", "answer"])
@@ -195,22 +200,24 @@ class VariableRenamingTrainer(Trainer[VariableRenamingConfig]):
         val_path = Path(self.config.dataset_path) / "val"
         if not os.path.exists(val_path):
             prompter = VariableRenamingPrompter(self.config)
-            prompter.make_dataset(val_path.as_posix())
+            prompter.make_dataset(val_path.as_posix(), size=self.config.val_size, seed=42)
 
         dataset = Dataset.load_from_disk(val_path.as_posix())
-        dataset.set_format(type="torch", columns=["prompt_tokens", "answer_tokens", "prompt", "answer"])
+        dataset.set_format(type="torch", columns=["prompt_tokens", "answer_tertokens", "prompt", "answer"])
         return torch.utils.data.DataLoader(dataset, batch_size=self.config.batch_size, shuffle=True)
 
 
 if __name__ == "__main__":
     config = VariableRenamingConfig(
-        num_prompts=1000,
-        num_chains=5,
-        chain_length=5,
+        train_size=100000,
+        val_size=1000,
+        num_chains=2,
+        chain_length=3,
         num_epochs=1000,
         batch_size=128,
         log_every_n_seconds=1,
         dataset_path="data/var_rename",
+        device="cuda" if torch.cuda.is_available() else "cpu",
     )
     trainer = VariableRenamingTrainer(config)
     trainer.run()

iluvattnshun/prompter.py
@@ -1,5 +1,4 @@
 import hashlib
-import os
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Generic, TypeVar
@@ -11,9 +10,6 @@ from datasets import Dataset, DatasetInfo, Features, Sequence, Value
 @dataclass(kw_only=True)
 class PromptConfig:
     """Base configuration class for prompt generation."""
-
-    num_prompts: int
-    seed: int = 42
 
     def __str__(self) -> str:
         """String representation of the config."""
@@ -41,7 +37,6 @@ class Prompter(ABC, Generic[ConfigType]):
 
     def __init__(self, config: ConfigType):
         """Initialize the synthesizer with a configuration."""
-        assert isinstance(config, PromptConfig), f"Config must inherit from PromptConfig, got {type(config)}"
         self.config = config
 
     @abstractmethod
@@ -61,19 +56,20 @@ class Prompter(ABC, Generic[ConfigType]):
         """
         pass
 
-    def make_dataset(self, path: str) -> None:
+    def make_dataset(self, path: str, size: int, seed: int = 42) -> None:
         """Generate a HuggingFace dataset of prompts and answers with config.
 
         Args:
             path: Path to save the dataset to.
+            seed: Seed for the random number generator.
         """
         prompts = []
         answers = []
         prompt_tokens = []
         answer_tokens = []
-        np.random.seed(self.config.seed)  # If ever do multiproc, rethink...
+        np.random.seed(seed)  # If ever do multiproc, rethink...
 
-        for _ in range(self.config.num_prompts):
+        for _ in range(size):
             prompt, answer = self.get_prompt()
             prompts.append(prompt)
             answers.append(answer)

iluvattnshun/trainer.py
@@ -1,18 +1,16 @@
 """Base trainer class."""
 
 from abc import ABC, abstractmethod
-from collections.abc import Sequence
 from dataclasses import dataclass
-from time import time
-from typing import Generic, Iterable, Iterator, Literal, TypeVar
+from typing import Generic, Iterable, TypeVar
 
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
 from iluvattnshun.logger import Logger
 from iluvattnshun.types import TensorTree
+from iluvattnshun.utils import move_to_device
 
 
 @dataclass(kw_only=True)
@@ -24,6 +22,8 @@ class TrainerConfig:
     """Number of training epochs. If -1, training will continue indefinitely."""
     batch_size: int
     """Batch size."""
+    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+    """Device to use for training."""
 
     # Logging
     log_every_n_seconds: float = 30.0
@@ -88,7 +88,7 @@ class Trainer(ABC, Generic[ConfigType]):
 
     def run(self) -> None:
         """Creates or loads training variables and begins training."""
-        model = self.get_model()
+        model = self.get_model().to(self.config.device)
         optimizer = self.get_optimizer(model)
         train_loader = self.get_train_dataloader()
         val_loader = self.get_val_dataloader()
@@ -97,6 +97,7 @@ class Trainer(ABC, Generic[ConfigType]):
             # Clasic batched training loop
             model.train()
             for batch in train_loader:
+                batch = move_to_device(batch, self.config.device)
                 metrics = self.train_step(model, optimizer, batch)
                 self.logger.log({"epoch": epoch, **metrics}, mode="train")
 
@@ -105,6 +106,7 @@ class Trainer(ABC, Generic[ConfigType]):
             eval_metrics: dict[str, float | str] = {}
             eval_size = 0
             for batch in val_loader:
+                batch = move_to_device(batch, self.config.device)
                 metrics = self.val_step(model, batch)
                 eval_metrics = {
                     k: eval_metrics.get(k, 0) + v if isinstance(v, float) else v for k, v in metrics.items()